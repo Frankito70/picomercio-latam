3f2197505fc769ed320becf3a5ba53766a1fa225
Frankito70 <buhito70@gmail.com>
2025-07-05 19:16:44 -0500

    Primer commit del proyecto PiComercio LATAM

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Bienvenidos a PiComercio LATAM</h1>
+      <p>Tu comercio con Pi en América Latina.</p>
+    </div>
+  )
+}
